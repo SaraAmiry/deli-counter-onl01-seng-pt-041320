f92f48dadf6fd81ce2c287a6123e4acaa5bb43b5
Sara Amiry <saraamiry24@gmail.com>
2020-04-25 06:25:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,12 @@ katz_deli = []
 def line(array)
   if array.count == 0
   print "The line is currently empty."
+else
+  say = "The line is currently:" array.each_with_index do |name, index|
+    say += "#{index+1}.#{name}"
+end
+puts say
+end
 end
 
 def take_a_number(katz_deli, " ")
