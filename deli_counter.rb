9caf66430237f51afb653a586f2c97ab5e5c4f9d
Sara Amiry <saraamiry24@gmail.com>
2020-04-25 06:28:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,11 +2,13 @@ katz_deli = []
 def line(array)
   if array.count == 0
   puts "The line is currently empty."
-else
-  say = "The line is currently:" array.each_with_index do |name, index|
+  else
+    say = "The line is currently:" 
+    array.each_with_index do |name, index|
     say += "#{index+1}.#{name}"
-end
-puts say
+    end
+  puts say
+  end
 end
 
 
